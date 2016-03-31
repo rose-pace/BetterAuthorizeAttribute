90e0d07ba11f4b0cc7f233afc0c174ecbf8286fc
Ross Pace <rpace@avantia-inc.com>
2016-03-31 16:55:57 -0400

    committing initial projects

---
+++ MVC.BetterAuthorizeAttribute/BetterAuthorizeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MVC.BetterAuthorizeAttribute
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
+    public class BetterAuthorizeAttribute : AuthorizeAttribute
+    {
+        const string COMMA = ",";
+        public string[] RoleList
+        {
+            get { return Roles.Split(new[] { COMMA }, StringSplitOptions.RemoveEmptyEntries); }
+            set { Roles = string.Join(COMMA, value); }
+        }
+
+        public string[] UserList
+        {
+            get { return Users.Split(new[] { COMMA }, StringSplitOptions.RemoveEmptyEntries); }
+            set { Users = string.Join(COMMA, value); }
+        }
+    }
+}
